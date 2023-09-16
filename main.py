c0e8c92cc125e3115cfa51c7f27c558198dc8f26
yosuke <yousuke.k2001@gmail.com>
2023-09-16 16:43:04 +0900

    :sparkles: [add] 入力するpdfと出力するpdf用のディレクトリ使う機能追加

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 import os
-from PyPDF2 import PdfFileMerger
+from PyPDF2 import PdfMerger
 
-# 同じディレクトリ内のPDFファイルのリストを取得
+# "input_pdf"ディレクトリに移動
+input_pdf_dir = "input_pdf"
+os.chdir(input_pdf_dir)
+
+# "input_pdf"ディレクトリ内のPDFファイルのリストを取得
 pdf_files = [file for file in os.listdir() if file.endswith('.pdf')]
 
 # PDFファイルが存在しない場合のエラーチェック
 if not pdf_files:
     print("ディレクトリ内にPDFファイルが見つかりません。")
 else:
-    # PDFファイルを結合するためのPdfFileMergerオブジェクトを作成
-    pdf_merger = PdfFileMerger()
+    # PDFファイルを結合するためのPdfMergerオブジェクトを作成
+    pdf_merger = PdfMerger()
 
     # 各PDFファイルを結合
     for pdf_file in pdf_files:
         pdf_merger.append(pdf_file)
 
-    # 結合したPDFを新しいファイルに保存
-    merged_pdf_name = 'merged.pdf'
+    # 結合したPDFを新しいファイルに保存（元のディレクトリに保存）
+    merged_pdf_name = '../output_pdf/output.pdf'  # ディレクトリに保存
     pdf_merger.write(merged_pdf_name)
 
     # 終了時に必ずファイルをクローズ
